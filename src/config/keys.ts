8bf46700ae64140af3d07c14b6b11ac103987f4a
Jeferson Arango L <jeferarlo2@gmail.com>
2021-10-25 16:41:15 -0500

    Carga y descarga de archivos.

---
+++ src/config/keys.ts
@@ -0,0 +1,9 @@
+export namespace Keys {
+  export const carpetaImagenProducto = "../../archivos/productos/";
+  export const nombreCampoImagenProducto = 'file';
+  export const extensionesPermitidasIMG: string[] = ['.PNG', '.JPG', '.JPEG', '.SVG'];
+  export const tamMaxImagenProducto = 1024 * 1024;
+  export const carpetaDocumentoPersona = '../../archivos/documentos';
+  export const nombreCampoDocumentoPersona = 'file';
+  export const extensionesPermitidasDOC: string[] = ['.PDF', '.DOC', '.DOCX', '.XLS', '.XLSX'];
+}
